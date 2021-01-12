86230e5e62ef4c7494da13a43a97d06b0fd68644
Felix Grohs <contact@felixgrohs.at>
2021-01-12 15:26:56 +0100

    add auto centering

--- src/fractals/FibonacciWordFractal.vue
+++ src/fractals/FibonacciWordFractal.vue
@@ -3,8 +3,6 @@
 		<template #settings>
 			<slider label="Step" v-model:current="settings.step" :max="1593" :min="0" />
 			<slider label="Line Length" v-model:current="settings.lineLength" :max="50" :min="0" />
-			<slider label="Padding Left" v-model:current="settings.paddingLeft" :max="width" :min="0" />
-			<slider label="Padding Top" v-model:current="settings.paddingTop" :max="height" :min="0" />
 		</template>
 
 		<canvas ref="canvas" id="canvas" />
@@ -12,8 +10,6 @@
 </template>
 
 <script>
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-empty */
 import Fractal from '@/mixins/Fractal'
 
 export default {
@@ -24,10 +20,8 @@ export default {
 			word: [],
 			dir: '',
 			settings: {
-				step: 985,
+				step: 580,
 				lineLength: 8,
-				paddingLeft: 120,
-				paddingTop: 300,
 			}
 		}
 	},
@@ -36,13 +30,9 @@ export default {
 	},
 	methods: {
 		init() {
-			const center = {
-				x: this.settings.paddingLeft,
-				y: this.settings.paddingTop
-			}
-
+			const sp = this.startPosition()
 			this.dir = 'bottom'
-			this.fibonacci(center, 0, this.settings.step)
+			this.fibonacci(sp, 0, this.settings.step)
 		},
 		fibonacci(pos, index, limit) {
 			const digit = this.word[index]
@@ -106,6 +96,28 @@ export default {
 				default:
 					return this.dir
 			}
+		},
+		startPosition() {
+			let pos = {}, relh, relw
+
+			relh = (this.settings.step <= 491) ? this.settings.step / (491 / 100) * 0.01 : 491 / (491 / 100) * 0.01
+
+			if(this.settings.step <= 114) {
+				relw = this.settings.step / (114 / 100) * 0.01
+			} else {
+				relw = 114 / (114 / 100) * 0.01
+			}
+
+			if(this.settings.step >= 600 && this.settings.step <= 984) {
+				relw += (this.settings.step - 600) / ((984 - 600) / 100) * 0.02 * 1.15
+			} else if (this.settings.step > 984) {
+				relw += (984 - 600) / ((984 - 600) / 100) * 0.02 * 1.15
+			}
+
+			pos.x = this.width / 2 - relw * (15 * this.settings.lineLength)
+			pos.y = this.height / 2 + relh * (33 * this.settings.lineLength)
+
+			return pos;
 		},
 		drawLine(p, direction) {
 			this.ctx.beginPath()
