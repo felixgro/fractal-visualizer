1d6de0c8ae5c8f0c5e047c140ea3f12f58445f94
Felix Grohs <contact@felixgrohs.at>
2021-02-06 16:03:05 +0100

    improved colors

--- src/config/Colors.js
+++ src/config/Colors.js
@@ -1,13 +1,15 @@
 export const fractalColors = [
 	'rgb(255, 255, 255)',
-	'green',
-	'blue',
-	'red'
+	'rgb(14, 15, 25)',
+	'rgb(4, 75, 217)',
+	'rgb(52, 194, 106)',
+	'rgb(189, 66, 120)',
 ]
 
 export const bgColors = [
+	'rgb(255, 255, 255)',
 	'rgb(14, 15, 25)',
-	'orange',
-	'pink',
-	'green'
+	'rgb(4, 75, 217)',
+	'rgb(52, 194, 106)',
+	'rgb(189, 66, 120)',
 ]
